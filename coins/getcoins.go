ad5dc5781a289699a95242ec1e6731e0f719520a
Arpit Kumar Rai <arpitkr20@iitk.ac.in>
2021-06-20 19:09:47 +0530

    Add functionality to check user coin balance

---
+++ coins/getcoins.go
@@ -0,0 +1,45 @@
+package coins
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type roll struct {
+	R int `json:"roll"`
+}
+
+func GetCoins(w http.ResponseWriter, r *http.Request) {
+	var rs roll
+	err := json.NewDecoder(r.Body).Decode(&rs)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Header().Set("Content-type", "json")
+		json.NewEncoder(w).Encode("Error while parsing credentials")
+		log.Print(err)
+		return
+	}
+
+	//Check if Roll number exists in database
+	if !CheckRoll(MyDB, rs.R, w) {
+		return
+	}
+
+	Mutex.Lock()
+
+	rows, err := MyDB.Query("Select Coins from User where Roll = ?", rs.R)
+	if err != nil {
+		SetError(w, err)
+		return
+	}
+	coins := 0
+	for rows.Next() {
+		rows.Scan(&coins)
+	}
+	Mutex.Unlock()
+	json.NewEncoder(w).Encode(fmt.Sprint("Coins :", coins))
+}
